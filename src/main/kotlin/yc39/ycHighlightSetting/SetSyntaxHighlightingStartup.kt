42388ded5b2de6bb11c0cb376580373cb62ca10d
yangweigeng <yangweigeng@issmart.com.cn>
2026-09-18 16:35:49 +0800

    fix 修复bug

--- src/main/kotlin/yc39/ycHighlightSetting/SetSyntaxHighlightingStartup.kt
+++ src/main/kotlin/yc39/ycHighlightSetting/SetSyntaxHighlightingStartup.kt
@@ -2,11 +2,12 @@ package yc39.ycHighlightSetting
 
 import com.intellij.codeInsight.daemon.impl.analysis.FileHighlightingSetting
 import com.intellij.codeInsight.daemon.impl.analysis.HighlightingSettingsPerFile
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.FileEditorManagerListener
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.openapi.startup.StartupActivity
+import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.psi.PsiManager
 
 
@@ -20,47 +21,36 @@ class SetSyntaxHighlightingStartup : StartupActivity {
         project.messageBus.connect().subscribe(
             FileEditorManagerListener.FILE_EDITOR_MANAGER,
             object : FileEditorManagerListener {
-                override fun fileOpened(source: FileEditorManager, file: com.intellij.openapi.vfs.VirtualFile) {
-                    if (!file.isDirectory && file.extension in setOf("java", "kt", "xml")) {
-                        println("fileOpened: $file")
-                        val psiFile = PsiManager.getInstance(project).findFile(file)
-                        if (psiFile != null) {
-                            val instance = HighlightingSettingsPerFile.getInstance(project)
-                            instance.setHighlightingSettingForRoot(psiFile, FileHighlightingSetting.SKIP_INSPECTION)
-                        }
-                    }
+                override fun fileOpened(source: FileEditorManager, file: VirtualFile) {
+                    applyHighlighting(source.project, file)
                 }
             }
         )
     }
 
     private fun applyHighlightingToOpenFiles(project: Project) {
-        val instance = HighlightingSettingsPerFile.getInstance(project)
         val openFiles = FileEditorManager.getInstance(project).openFiles
-
         for (file in openFiles) {
-            if (!file.isDirectory && file.extension in setOf("java", "kt", "xml")) {
-                println("fileOpened: $file")
-                val psiFile = PsiManager.getInstance(project).findFile(file)
-                if (psiFile != null) {
-                    instance.setHighlightingSettingForRoot(psiFile, FileHighlightingSetting.SKIP_INSPECTION)
-                }
-            }
+            applyHighlighting(project, file)
         }
     }
 
-//    override fun runActivity(project: Project) {
-//        val instance = HighlightingSettingsPerFile.getInstance(project)
-//        ProjectRootManager.getInstance(project).fileIndex.iterateContent { file ->
-//            if (!file.isDirectory && file.extension in setOf("java", "kt", "xml")) {
-//                val psiFile = PsiManager.getInstance(project).findFile(file)
-//                if (psiFile != null) {
-//                    instance.setHighlightingSettingForRoot(psiFile, FileHighlightingSetting.SKIP_INSPECTION)
-//                }
-//            }
-//            true
-//        }
-//    }
+    /**
+     * setHighlightingSettingForRoot() fires a synchronous message bus event that runs a write
+     * action (DaemonListeners -> dropPsiCaches). It must be called from a write-safe context,
+     * otherwise TransactionGuard throws "Write-unsafe context!". Defer via invokeLater.
+     */
+    private fun applyHighlighting(project: Project, file: VirtualFile) {
+        if (file.isDirectory || file.extension !in TARGET_EXTENSIONS) return
+        ApplicationManager.getApplication().invokeLater {
+            if (project.isDisposed) return@invokeLater
+            val psiFile = PsiManager.getInstance(project).findFile(file) ?: return@invokeLater
+            val instance = HighlightingSettingsPerFile.getInstance(project)
+            instance.setHighlightingSettingForRoot(psiFile, FileHighlightingSetting.SKIP_INSPECTION)
+        }
+    }
 
-
+    companion object {
+        private val TARGET_EXTENSIONS = setOf("java", "kt", "xml")
+    }
 }
